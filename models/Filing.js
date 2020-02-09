c4b1026a432a5946723d2604519447a11b3629fc
kierankay <kierankay@gmail.com>
2020-02-08 19:02:50 -0800

    Add new filing model boilerplate

---
+++ models/Filing.js
@@ -0,0 +1,9 @@
+const db = require('../db');
+
+class Filings {
+    static async getByCompanyTicker(ticker) {
+        let upperticker = ticker.toUpperCase();
+    }
+}
+
+module.exports = Filings;
