10f3c627280afbe581bcc6968f5e4d08a6bf3518
kierankay <kierankay@gmail.com>
2020-02-09 10:33:47 -0800

    Finish loadByCompanyTicker method
    Method fetches and formats filing data from EDGAR for eventual database insertion

--- models/Filing.js
+++ models/Filing.js
@@ -1,9 +1,22 @@
 const db = require('../db');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 class Filings {
-    static async getByCompanyTicker(ticker) {
-        let upperTicker = ticker.toUpperCase();
-        return upperTicker
+    static async loadByCompanyTicker(ticker) {
+        let result = await axios.get(`https://www.sec.gov/cgi-bin/browse-edgar?CIK=${ticker}&Find=Search&owner=exclude&action=getcompany`);
+        let htmlBody = result.data;
+        let $ = cheerio.load(htmlBody);
+        let filingTable = $('.tableFile2').children().children();
+        let filings = filingTable.map((i, e) => ({
+            'filing': $(e).children().html().trim(),
+            'link': decodeURI($(e).children().nextAll().html().trim().split(/"/)[1]),
+            'description': $(e).children().nextAll().nextAll().text().trim().split(/\s{2,}/)[0],
+            'filing_date': $(e).children().nextAll().nextAll().nextAll().html().trim(),
+            'file_film_link': decodeURI($(e).children().nextAll().nextAll().nextAll().nextAll().html().trim().split(/"/)[1]),
+            'file_film_number': $(e).children().nextAll().nextAll().nextAll().nextAll().text().trim(),
+        })).get();
+        return filings
     }
 }
 
