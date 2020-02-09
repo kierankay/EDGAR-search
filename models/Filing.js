8585a8aba371b0548020d9f27c8621e10de7a7d6
kierankay <kierankay@gmail.com>
2020-02-09 11:23:04 -0800

    remove redundant company term from loadByTicker method

--- models/Filing.js
+++ models/Filing.js
@@ -3,20 +3,20 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 class Filings {
-    static async loadByCompanyTicker(ticker) {
-        let result = await axios.get(`https://www.sec.gov/cgi-bin/browse-edgar?CIK=${ticker}&Find=Search&owner=exclude&action=getcompany`);
-        let htmlBody = result.data;
-        let $ = cheerio.load(htmlBody);
-        let filingTable = $('.tableFile2').children().children();
+    static async loadByTicker(ticker) {
+        let searchResult = await axios.get(`https://www.sec.gov/cgi-bin/browse-edgar?CIK=${ticker}&Find=Search&owner=exclude&action=getcompany`);
+        let searchHtmlBody = searchResult.data;
+        let $s = cheerio.load(searchHtmlBody);
+        let filingTable = $s('.tableFile2').children().children();
         let filings = filingTable.map((i, e) => ({
-            'filing': $(e).children().html().trim(),
-            'link': decodeURI($(e).children().nextAll().html().trim().split(/"/)[1]),
-            'description': $(e).children().nextAll().nextAll().text().trim().split(/\s{2,}/)[0],
-            'filing_date': $(e).children().nextAll().nextAll().nextAll().html().trim(),
-            'file_film_link': decodeURI($(e).children().nextAll().nextAll().nextAll().nextAll().html().trim().split(/"/)[1]),
-            'file_film_number': $(e).children().nextAll().nextAll().nextAll().nextAll().text().trim(),
+            'filing': $s(e).children().html().trim(),
+            'link': decodeURI($s(e).children().nextAll().html().trim().split(/"/)[1]),
+            'description': $s(e).children().nextAll().nextAll().text().trim().split(/\s{2,}/)[0],
+            'filing_date': $s(e).children().nextAll().nextAll().nextAll().html().trim(),
+            'file_film_link': decodeURI($s(e).children().nextAll().nextAll().nextAll().nextAll().html().trim().split(/"/)[1]),
+            'file_film_number': $s(e).children().nextAll().nextAll().nextAll().nextAll().text().trim(),
         })).get();
-        return filings
+        return filings;
     }
 }
 
