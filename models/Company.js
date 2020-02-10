c25279a006024db0343970ee2aaa5006e25bbd50
kierankay <kierankay@gmail.com>
2020-02-09 22:40:31 -0800

    Add new methods to load, parse, save and fetch ticker data

--- models/Company.js
+++ models/Company.js
@@ -4,23 +4,47 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 class Companies {
-    static async loadFromFile(filePath) {
-        let companyFilings = fs.readFileSync('./data/xbrl.idx', 'utf8').toString().split(/\n/).map(e => ({
-            'cik': parseInt(e.split('|')[0]),
-            'companyName': e.split('|')[1],
-            'filing': e.split('|')[2],
-            'date': e.split('|')[3],
-            'folder': e.split('|')[4] ? e.split('|')[4].split(/[/|\-|.]/).reduce((r,e,i) => (i === 2 || i === 5) ? r + e + '/' : (i === 3 || i === 4) ? r + e : r + '', '/') : 0
-        }));
-        console.log(companyFilings);
+    // static async loadFromFile(filePath) {
+    //     let companies = fs.readFileSync('./data/cik_ticker.csv', 'utf8').toString().split(/\n/).map(e => ({
+    //         'cik': parseInt(e.split('|')[0]),
+    //         'ticker': e.split('|')[1],
+    //         'companyName': e.split('|')[2]
+    //       }));
+    //     Companies.add(companies);
+    //     // console.log(companies);
+    // }
+
+    static async getCik(ticker) {
+        let result = await db.query(`
+        SELECT *
+        FROM companies
+        WHERE ticker = $1`,
+            [ticker]);
+        let cik = result.rows[0].cik;
+        return cik;
     }
 
-    static async addFiling(cik, filing, date, filePath) {
+    static async loadFromFile(filePath) {
+        let companies = fs.readFileSync('./data/ticker.txt', 'utf8').toString().split(/\n/).map(e => ({
+            'cik': parseInt(e.split(/\s+/)[1]),
+            'ticker': e.split(/\s+/)[0]
+        }));
+        Companies.add(companies);
+    }
+
+    static async addOne(company) {
+        let { cik, ticker } = company
         let result = await db.query(`
-        INSERT INTO filings
-        (cik, filing, date, filePath)`)
+        INSERT INTO companies
+        (cik, ticker)
+        VALUES ($1, $2)`, [cik, ticker]
+        );
+    }
+
+    static async add(companies) {
+        companies.forEach((e) => (Companies.addOne(e)));
     }
 
 }
 
-module.exports = Filings;
+module.exports = Companies;
