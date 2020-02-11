69162f8fbc7bcf80fab8fdc5cdd42a5588582626
kierankay <kierankay@gmail.com>
2020-02-10 16:07:08 -0800

    Add GET /build/filings and GET /build/tickers
    -...tickers uses Company methods to fetch tickers from SEC.gov, parse, and insert ticker-CIK mappings into the tickers table
    ...filings uses Filing methods to build filings URLs, fetch filings from EDGAR, parse, and insert them into the filings table

--- routes/filingRoutes.js
+++ routes/filingRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Filings = require('../models/Filing');
 const Companies = require('../models/Company');
+const fs = require('fs');
 
 const router = express.Router();
 
@@ -10,6 +11,33 @@ const router = express.Router();
   Fetch a filings for a company by stock ticker
   ex: GET /api/filings/company/ticker/AAPL
 */
+
+router.get('/build/tickers', async function (req, res, next) {
+    let loadFrom = 'https://www.sec.gov/include/ticker.txt';
+    let saveTo = './data/tickers';
+    // fetch tickers from SEC.gov
+    await Companies.load(loadFrom, saveTo);
+
+    // parse and load into database
+    await Companies.loadFromFile(saveTo);
+    return res.json({"message": "built tickers"});
+});
+
+router.get('/build/filings', async function (req, res, next) {
+    let baseUrl = 'https://www.sec.gov/Archives/edgar/full-index/';
+    let baseDataFolder = './data/xbrls'
+    let filingUrls = await Filings.buildFilingsURLs(baseUrl);
+
+    // build all xbrl lists at ./data/xbrls
+    // await Filings.fetchAllFilings(filingUrls);
+
+    let files = fs.readdirSync(baseDataFolder);
+
+    for (let file of files) {
+        await Filings.loadFromFile(`${baseDataFolder}/${file}`);
+    }
+    return res.json({"message": "built files"});
+});
 
 router.get('/ticker/:ticker', async function (req, res, next) {
     try {
