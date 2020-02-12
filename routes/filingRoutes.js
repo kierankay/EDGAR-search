e42ae2ebeb3835a921ce10d97fe42c53ecbe575d
kierankay <kierankay@gmail.com>
2020-02-11 16:20:13 -0800

    Rename Filings to Forms and add first cut of GET /:id

--- routes/filingRoutes.js
+++ routes/filingRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const Filings = require('../models/Filing');
+const Forms = require('../models/Filing');
 const Companies = require('../models/Company');
 const fs = require('fs');
+
+let baseUrl = 'https://www.sec.gov/Archives/edgar/data';
+let baseXbrlUrl = 'https://www.sec.gov/ix?doc=/Archives/edgar/data';
 
 const router = express.Router();
 
@@ -13,30 +16,38 @@ const router = express.Router();
 */
 
 router.get('/build/tickers', async function (req, res, next) {
-    let loadFrom = 'https://www.sec.gov/include/ticker.txt';
-    let saveTo = './data/tickers';
-    // fetch tickers from SEC.gov
-    await Companies.load(loadFrom, saveTo);
+    try {
+        let loadFrom = 'https://www.sec.gov/include/ticker.txt';
+        let saveTo = './data/tickers';
+        // fetch tickers from SEC.gov
+        await Companies.load(loadFrom, saveTo);
 
-    // parse and load into database
-    await Companies.loadFromFile(saveTo);
-    return res.json({"message": "built tickers"});
+        // parse and load into database
+        let companyFilings = await Companies.loadFromFile(saveTo);
+        await Forms.add(companyFilings);
+        return res.json({ "message": "built tickers" });
+    } catch (err) {
+        next(err);
+    }
 });
 
 router.get('/build/filings', async function (req, res, next) {
-    let baseUrl = 'https://www.sec.gov/Archives/edgar/full-index/';
-    let baseDataFolder = './data/xbrls'
-    let filingUrls = await Filings.buildFilingsURLs(baseUrl);
+    try {
+        let baseDataFolder = './data/xbrls'
+        let formListUrls = await Forms.buildFormListUrls(baseUrl);
 
-    // build all xbrl lists at ./data/xbrls
-    // await Filings.fetchAllFilings(filingUrls);
+        // build all xbrl lists at ./data/xbrls
+        // await Forms.getFormLists(formListUrls);
 
-    let files = fs.readdirSync(baseDataFolder);
-
-    for (let file of files) {
-        await Filings.loadFromFile(`${baseDataFolder}/${file}`);
+        let files = fs.readdirSync(baseDataFolder);
+        for (let formList of files) {
+            let companyForms = await Forms.loadFormList(`${baseDataFolder}/${formList}`);
+            await Forms.add(companyForms);
+        }
+        return res.json({ "message": "built forms" });
+    } catch (err) {
+        next(err)
     }
-    return res.json({"message": "built files"});
 });
 
 router.get('/ticker/:ticker', async function (req, res, next) {
@@ -47,14 +58,31 @@ router.get('/ticker/:ticker', async function (req, res, next) {
         let cik = await Companies.getCik(ticker);
 
         // get the filing from the CIK number
-        let filing = await Filings.get(cik);
+        let forms = await Forms.getByCik(cik);
 
         // sort and return as a JSON object
-        filing.sort((a,b) => b.date_filed - a.date_filed);
-        let response = { ticker, filings: filing }
+        forms.sort((a, b) => b.date_filed - a.date_filed);
+        let response = { ticker, forms }
+        let namedForms = await Forms.getFormNames(forms, baseUrl);
+        await Forms.updateFormFileNames(namedForms);
         return res.json(response);
     } catch (err) {
         return next(err);
+    }
+});
+
+// redirects to financial statement link if first time requested
+router.get('/:id', async function (req, res, next) {
+    try {
+        let id = req.params.id;
+        let filing = await Forms.getById(id);
+        if (filing.file_url === null) {
+            return res.json({ "message": "file not found" });
+        }
+        let url = `${baseXbrlUrl}${filing.file_location}${filing.file_url}`
+        return res.redirect(url);
+    } catch (err) {
+        next(err)
     }
 });
 
