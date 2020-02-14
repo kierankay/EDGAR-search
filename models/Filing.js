46a9347210ffc4e6478a4230900ce96a80211895
kierankay <kierankay@gmail.com>
2020-02-13 21:47:24 -0800

    Add docstrings for readability

--- models/Filing.js
+++ models/Filing.js
@@ -9,24 +9,6 @@ let baseTimeoutMs = 1000;
 
 class Forms {
 
-    // Backup function to load filings directly from SEC website
-    // Used in case our library is missing something. We should log the use of this function.
-    static async loadByTicker(ticker) {
-        let searchResult = await axios.get(`https://www.sec.gov/cgi-bin/browse-edgar?CIK=${ticker}&Find=Search&owner=exclude&action=getcompany`);
-        let searchHtmlBody = searchResult.data;
-        let $s = cheerio.load(searchHtmlBody);
-        let filingTable = $s('.tableFile2').children().children();
-        let filings = filingTable.map((i, e) => ({
-            'filing': $s(e).children().html().trim(),
-            'link': decodeURI($s(e).children().nextAll().html().trim().split(/"/)[1]),
-            'description': $s(e).children().nextAll().nextAll().text().trim().split(/\s{2,}/)[0],
-            'filing_date': $s(e).children().nextAll().nextAll().nextAll().html().trim(),
-            'file_film_link': decodeURI($s(e).children().nextAll().nextAll().nextAll().nextAll().html().trim().split(/"/)[1]),
-            'file_film_number': $s(e).children().nextAll().nextAll().nextAll().nextAll().text().trim(),
-        })).get();
-        return filings;
-    }
-
     // Return an array of URLs pointing to quarterly lists of xbrl filing links
     static async buildFormListURLs(baseUrl) {
         let currYear = new Date().getFullYear();
@@ -203,6 +185,24 @@ class Forms {
             }
         })
         return url[0] ? url[0].name : null;
+    }
+
+    // Backup function to load filings directly from SEC website
+    // Used in case our library is missing something. We should log the use of this function.
+    static async loadByTicker(ticker) {
+        let searchResult = await axios.get(`https://www.sec.gov/cgi-bin/browse-edgar?CIK=${ticker}&Find=Search&owner=exclude&action=getcompany`);
+        let searchHtmlBody = searchResult.data;
+        let $s = cheerio.load(searchHtmlBody);
+        let filingTable = $s('.tableFile2').children().children();
+        let filings = filingTable.map((i, e) => ({
+            'filing': $s(e).children().html().trim(),
+            'link': decodeURI($s(e).children().nextAll().html().trim().split(/"/)[1]),
+            'description': $s(e).children().nextAll().nextAll().text().trim().split(/\s{2,}/)[0],
+            'filing_date': $s(e).children().nextAll().nextAll().nextAll().html().trim(),
+            'file_film_link': decodeURI($s(e).children().nextAll().nextAll().nextAll().nextAll().html().trim().split(/"/)[1]),
+            'file_film_number': $s(e).children().nextAll().nextAll().nextAll().nextAll().text().trim(),
+        })).get();
+        return filings;
     }
 }
 
