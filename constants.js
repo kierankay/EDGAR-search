6b10ddab053edc3f89123307f9b2432018217c1a
kierankay <kierankay@gmail.com>
2020-02-14 00:09:03 -0800

    Moved all global constants here

---
+++ constants.js
@@ -0,0 +1,10 @@
+let constants = {
+    baseArchiveUrl: 'https://www.sec.gov/Archives/edgar/data',
+    baseXbrlUrl: 'https://www.sec.gov/ix?doc:/Archives/edgar/data',
+    baseXbrlListSavePath: './data/xbrls',
+    baseTickerUrl: 'https://www.sec.gov/include/ticker.txt',
+    baseTickerSavePath: './data/tickers',
+    baseTimeoutMs: 1000
+}
+
+module.exports = constants;
