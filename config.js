202b0e115359b5ff6ea325a8650c6a0a4a787905
kierankay <kierankay@gmail.com>
2020-02-14 00:22:21 -0800

    Rename database from databook to edgar-search to decouple from databook project

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ let {
 } = process.env
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = `databook-test`
+  DB_URI = `edgar-search-test`
 } else {
-  DB_URI = process.env.DATABASE_URL || `databook`;
+  DB_URI = process.env.DATABASE_URL || `edgar-search`;
 }
 
 module.exports = { DB_URI, PGUSER, PGPASSWORD, BACKENDSERVER, FRONTENDSERVER }
