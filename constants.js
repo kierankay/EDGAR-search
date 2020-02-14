d51577ac07184401818354de1a3b61fd25a2041d
kierankay <kierankay@gmail.com>
2020-02-14 02:11:14 -0800

    Fixed a bug by replacing ix?doc: with id?doc= that was breaking the URL

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 let constants = {
     baseIdxArchiveUrl: 'https://www.sec.gov/Archives/edgar/full-index',
     baseArchiveUrl: 'https://www.sec.gov/Archives/edgar/data',
-    baseXbrlUrl: 'https://www.sec.gov/ix?doc:/Archives/edgar/data',
+    baseXbrlUrl: 'https://www.sec.gov/ix?doc=/Archives/edgar/data',
     baseXbrlListSavePath: './data/xbrls',
     baseTickerUrl: 'https://www.sec.gov/include/ticker.txt',
     baseTickerSavePath: './data/tickers',
