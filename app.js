872daa1a9db26ab6cc77dc2129e20326b6ff6a12
kierankay <kierankay@gmail.com>
2020-02-20 19:57:42 -0800

    Installed ESLint and fixed errors

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
-const ExpressError = require('./helpers/expressError');
-
+const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
+const ExpressError = require('./helpers/expressError');
+const formRoutes = require('./routes/formRoutes');
+const { tickerJob, xbrlJob } = require('./cron-jobs/dailyCron');
+
 dotenv.config();
 
-const express = require('express');
 const app = express();
-
-const cors = require('cors');
 app.use(cors());
 app.options('*', cors());
-
-const formRoutes = require('./routes/formRoutes');
 app.use('/api/forms', formRoutes);
 
-const { tickerJob, xbrlJob } = require('./cron-jobs/dailyCron');
-tickerJob.start();
-xbrlJob.start();
-
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   const err = new ExpressError('resource not found', 404);
   return next(err);
-})
+});
 
-app.use(function (err, req, res, next) {
+app.use((err, req, res) => {
   res.status(err.status || 500);
-  if (process.env.NODE_ENV != "test") {
+  if (process.env.NODE_ENV !== 'test') {
     console.error(err.stack);
   }
 
   return res.json({
     error: err,
-    message: err.message
+    message: err.message,
   });
 });
+
+tickerJob.start();
+xbrlJob.start();
+
 
 module.exports = app;
